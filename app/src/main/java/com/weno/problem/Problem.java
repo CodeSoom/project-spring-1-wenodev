0748de369f2ad27de9b287e2df545e65f50c3880
wenodev <logicalstark1105@gmail.com>
2021-03-24 08:22:04 +0900

    feat: Problem 테스트를 위한 fixture 생성하라

---
+++ app/src/main/java/com/weno/problem/Problem.java
@@ -0,0 +1,4 @@
+package com.weno.problem;
+
+public class Problem {
+}
---
+++ app/src/test/java/com/weno/problem/ProblemTest.java
@@ -0,0 +1,16 @@
+package com.weno.problem;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProblemTest {
+
+    private Problem problem;
+
+    @BeforeEach
+    void setUp(){
+        problem = new Problem();
+    }
+
+}
