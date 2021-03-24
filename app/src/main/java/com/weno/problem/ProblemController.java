4e39f534cb705e946ca149e69bba32e80a473af2
wenodev <logicalstark1105@gmail.com>
2021-03-25 08:06:19 +0900

    test: getAllProblems 성공하라

--- app/src/main/java/com/weno/problem/ProblemController.java
+++ app/src/main/java/com/weno/problem/ProblemController.java
@@ -1,4 +1,14 @@
 package com.weno.problem;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/v1/problems")
+@RestController
 public class ProblemController {
+
+    @GetMapping
+    public void getAllProblems(){
+    }
 }
