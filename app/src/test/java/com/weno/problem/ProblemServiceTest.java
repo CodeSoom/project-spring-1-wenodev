93a437d8d743a4d2c2ec5a569bbf86d4bcb17dca
wenodev <logicalstark1105@gmail.com>
2021-03-25 08:10:53 +0900

    feat: ProductRepository 생성하라

--- app/src/test/java/com/weno/problem/ProblemServiceTest.java
+++ app/src/test/java/com/weno/problem/ProblemServiceTest.java
@@ -1,7 +1,16 @@
 package com.weno.problem;
 
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class ProblemServiceTest {
-    
+
+    @InjectMocks
+    private ProblemService problemService;
+
+    @Mock
+    private ProblemRepository problemRepository;
+
 }
