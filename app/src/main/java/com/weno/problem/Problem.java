3cfa9bf50303f6502a7bbaf844e3370009789105
wenodev <logicalstark1105@gmail.com>
2021-03-25 07:53:46 +0900

    feat: h2 db에 Probelm 테이블을 생성하라

--- app/src/main/java/com/weno/problem/Problem.java
+++ app/src/main/java/com/weno/problem/Problem.java
@@ -1,8 +1,24 @@
 package com.weno.problem;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Problem {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
     private String title;
+
+    public Problem() {
+        this.id = id;
+    }
 
     public Problem(String title) {
         this.title = title;
@@ -11,4 +27,5 @@ public class Problem {
     public String getTitle() {
         return title;
     }
+
 }
