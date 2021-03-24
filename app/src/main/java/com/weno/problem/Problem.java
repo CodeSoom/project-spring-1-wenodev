a48508525ecfe7f54491ab6d1c4228f46c1874fc
wenodev <logicalstark1105@gmail.com>
2021-03-25 07:55:51 +0900

    feat: lombok을 추가하여 코드를 수정하라
    Getter, NoargsConstructor, Builer를 사용한다.

--- app/src/main/java/com/weno/problem/Problem.java
+++ app/src/main/java/com/weno/problem/Problem.java
@@ -1,11 +1,17 @@
 package com.weno.problem;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Getter
+@NoArgsConstructor
 @Entity
 public class Problem {
 
@@ -16,16 +22,9 @@ public class Problem {
     @Column
     private String title;
 
-    public Problem() {
-        this.id = id;
-    }
-
+    @Builder
     public Problem(String title) {
         this.title = title;
-    }
-
-    public String getTitle() {
-        return title;
     }
 
 }
