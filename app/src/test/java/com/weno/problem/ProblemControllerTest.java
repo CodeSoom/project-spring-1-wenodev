0bfdae0ed66cc1f82a222abb02ec7577f219fa48
wenodev <logicalstark1105@gmail.com>
2021-03-29 22:05:39 +0900

    test: getProblem 실패하라

--- app/src/test/java/com/weno/problem/ProblemControllerTest.java
+++ app/src/test/java/com/weno/problem/ProblemControllerTest.java
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -15,16 +16,21 @@ class ProblemControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private String BASE_URL;
+    @MockBean
+    private ProblemService problemService;
 
-    @BeforeEach
-    void setUp(){
-        BASE_URL = "/api/v1/problems";
-    }
+    private static final Long EXITED_ID = 1L;
+    private static final String BASE_URL = "/api/v1/problems";
 
     @Test
     void testGetAllProblems() throws Exception {
         mockMvc.perform(get(BASE_URL))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    void testGetProblem() throws Exception {
+        mockMvc.perform(get(BASE_URL + "/" + EXITED_ID))
                 .andExpect(status().isOk());
     }
 }
