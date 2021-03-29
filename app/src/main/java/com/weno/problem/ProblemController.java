4c1f47de504bad97b2d62add60c37a80857b7c8c
wenodev <logicalstark1105@gmail.com>
2021-03-30 08:21:18 +0900

    feat: ProblemRequestDto 생성하라

--- app/src/main/java/com/weno/problem/ProblemController.java
+++ app/src/main/java/com/weno/problem/ProblemController.java
@@ -1,9 +1,11 @@
 package com.weno.problem;
 
+import com.weno.problem.dto.ProblemRequestDto;
 import com.weno.problem.dto.ProblemResponseDto;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,5 +29,9 @@ public class ProblemController {
     @GetMapping("/{id}")
     public ProblemResponseDto getProblem(@PathVariable Long id){
         return problemService.getProblem(id);
+    }
+
+    @PostMapping
+    public ProblemResponseDto saveProblem(@RequestBody ProblemRequestDto request){
     }
 }
---
+++ app/src/main/java/com/weno/problem/dto/ProblemRequestDto.java
@@ -0,0 +1,8 @@
+package com.weno.problem.dto;
+
+import lombok.Getter;
+
+@Getter
+public class ProblemRequestDto {
+    private String title;
+}
