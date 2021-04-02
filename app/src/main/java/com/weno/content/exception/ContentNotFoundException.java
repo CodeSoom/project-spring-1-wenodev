c82c0a3d38dc904d2d9917d00c5b4b0112e3d61e
weno <logicalstark1105@gmail.com>
2021-04-02 17:59:39 +0900

    feat: ContentNotFoundException 생성하라

---
+++ app/src/main/java/com/weno/content/exception/ContentNotFoundException.java
@@ -0,0 +1,11 @@
+package com.weno.content.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ContentNotFoundException extends RuntimeException {
+    public ContentNotFoundException(String s) {
+        super(s);
+    }
+}
