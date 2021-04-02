c1ea2070b0189227b620aab3a74b095fe56d624b
weno <logicalstark1105@gmail.com>
2021-04-02 17:38:12 +0900

    feat: ContentService 생성하라

---
+++ app/src/main/java/com/weno/content/ContentService.java
@@ -0,0 +1,11 @@
+package com.weno.content;
+
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@Transactional
+@Service
+public class ContentService {
+
+}
---
+++ app/src/test/java/com/weno/content/ContentServiceTest.java
@@ -0,0 +1,15 @@
+package com.weno.content;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ContentServiceTest {
+
+    private ContentService contentService;
+    private ContentRepository contentRepository;
+
+    @BeforeEach
+    void setUp() {
+    }
+}
