3422a0647a918dd7b5dc00702e6f346a80ccd98b
weno <logicalstark1105@gmail.com>
2021-04-02 17:55:34 +0900

    feat: ContentResponseDto 변수 생성하라

--- app/src/main/java/com/weno/content/dto/ContentResponseDto.java
+++ app/src/main/java/com/weno/content/dto/ContentResponseDto.java
@@ -1,4 +1,22 @@
 package com.weno.content.dto;
 
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@Getter
 public class ContentResponseDto {
+    private Long id;
+    private String question;
+    private String answer;
+    private String userAnswer;
+
+    @Builder
+    public ContentResponseDto(Long id, String question, String answer, String userAnswer) {
+        this.id = id;
+        this.question = question;
+        this.answer = answer;
+        this.userAnswer = userAnswer;
+    }
 }
