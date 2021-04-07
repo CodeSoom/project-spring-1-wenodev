4adae1e8bcd08a9dcf3c053a461f82df269f529a
wenodev <logicalstark1105@gmail.com>
2021-04-07 20:27:26 +0900

    feat: getContent 생성하라

--- app/src/main/java/com/weno/content/ContentController.java
+++ app/src/main/java/com/weno/content/ContentController.java
@@ -1,7 +1,12 @@
 package com.weno.content;
 
+import com.weno.content.dto.ContentResponseDto;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/api/v1/content")
 @RestController
 public class ContentController {
     private final ContentService contentService;
@@ -9,4 +14,10 @@ public class ContentController {
     public ContentController(ContentService contentService) {
         this.contentService = contentService;
     }
+
+    @GetMapping("/{id}")
+    public ContentResponseDto getContent(@PathVariable Long id){
+        return contentService.getContent(id);
+    }
+
 }
--- app/src/test/java/com/weno/content/ContentControllerTest.java
+++ app/src/test/java/com/weno/content/ContentControllerTest.java
@@ -3,12 +3,14 @@ package com.weno.content;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+@WebMvcTest(ContentController.class)
 class ContentControllerTest {
 
     @Autowired
