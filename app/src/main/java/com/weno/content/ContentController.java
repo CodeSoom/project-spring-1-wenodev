26406c54f36ac56f9d75cb92bb86270005961825
wenodev <logicalstark1105@gmail.com>
2021-04-07 20:49:26 +0900

    feat: saveContent 생성하라

--- app/src/main/java/com/weno/content/ContentController.java
+++ app/src/main/java/com/weno/content/ContentController.java
@@ -1,9 +1,14 @@
 package com.weno.content;
 
+import com.weno.content.dto.ContentRequestDto;
 import com.weno.content.dto.ContentResponseDto;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,6 +30,12 @@ public class ContentController {
     @GetMapping
     public List<ContentResponseDto> getAllContent(){
         return contentService.getAllContents();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping
+    public ContentResponseDto saveContent(@RequestBody ContentRequestDto request){
+        return contentService.saveContent(request);
     }
 
 }
