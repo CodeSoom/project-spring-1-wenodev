b08e818906494c2369756613961d36a067be87ab
wenodev <logicalstark1105@gmail.com>
2021-04-09 08:29:32 +0900

    fix: ProblemResponseDto에 contentResponseDto를 리턴하도록 하라

--- app/src/main/java/com/weno/problem/dto/ProblemResponseDto.java
+++ app/src/main/java/com/weno/problem/dto/ProblemResponseDto.java
@@ -4,20 +4,29 @@ import com.weno.content.dto.ContentResponseDto;
 import com.weno.problem.Problem;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 
-@Builder
+@NoArgsConstructor
 @Getter
 public class ProblemResponseDto {
     private Long id;
     private String title;
     private List<ContentResponseDto> contents;
 
+    @Builder
+    public ProblemResponseDto(Long id, String title, List<ContentResponseDto> contents) {
+        this.id = id;
+        this.title = title;
+        this.contents = contents;
+    }
+
     public static ProblemResponseDto of(Problem problem, List<ContentResponseDto> contents){
         return ProblemResponseDto.builder()
                 .id(problem.getId())
                 .title(problem.getTitle())
+                .contents(contents)
                 .build();
     }
 }
