a7a36a59a1c33906249fa2ffdb5f9106386a2ac9
weno <logicalstark1105@gmail.com>
2021-04-13 20:01:38 +0900

    feat: problems를 생성하라

--- app-web/src/components/ProblemListPage.jsx
+++ app-web/src/components/ProblemListPage.jsx
@@ -2,26 +2,10 @@ import React, { useState, useEffect } from 'react';
 
 export default function ProblemListPage() {
 
-    const [problems, setProblems] = useState(
-        [
-            {id : 1, title : "t1"},
-            {id : 2, title : "t2"}
-        ]
-    );
-
-
-    useEffect(() =>{
-
-    })
+    const [problems, setProblems] = useState(null);
 
     return(
         <ul>
-            dd
-            {problems.map((problem) => (
-                <li key={problem.id}>
-                    {problem.name}
-                </li>
-            ))}
         </ul>    
     );
 }
