7a8a0e1142c29daf8e5449b60f82dae5a29ceaa4
weno <logicalstark1105@gmail.com>
2021-04-13 21:48:35 +0900

    fix: 저장된 Problem을 불러와라

--- app-web/src/services/api.js
+++ app-web/src/services/api.js
@@ -1,7 +1,6 @@
-import React from "react";
-
 export async function fetchProblems() {
   const url = "http://localhost:8080/api/v1/problems";
   const response = await fetch(url);
-  return await response.json;
+  const data = await response.json();
+  return data;
 }
