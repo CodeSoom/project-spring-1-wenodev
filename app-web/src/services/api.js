c98697353a87f0ad3d1b5d746a3e4572b3182465
wenodev <logicalstark1105@gmail.com>
2021-04-14 01:17:12 +0900

    feat: ProductDetail 추가하라

--- app-web/src/services/api.js
+++ app-web/src/services/api.js
@@ -4,3 +4,11 @@ export async function fetchProblems() {
   const data = await response.json();
   return data;
 }
+
+export async function fetchProblem(id) {
+  const url = "http://localhost:8080/api/v1/problems/" + id;
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
