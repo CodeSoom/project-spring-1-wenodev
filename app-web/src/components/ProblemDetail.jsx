1d500c663696f2614a74c6ba899d344ba2505049
weno <logicalstark1105@gmail.com>
2021-04-14 23:31:23 +0900

    fix: 사용자가 작성한 답안의 데이터를 백엔드 dto의 형태로 정제하라

--- app-web/src/components/ProblemDetail.jsx
+++ app-web/src/components/ProblemDetail.jsx
@@ -6,6 +6,7 @@ import * as API from '../services/api'
  function ProblemDetail({match}){
 
     const[problem, setProblem] = useState(null);
+    const[answer, setAnswer] = useState([]);
 
     useEffect(() =>{
         API.fetchProblem(match.params.id)
@@ -18,6 +19,23 @@ import * as API from '../services/api'
    
     }, [])
 
+    function onChange(event){
+        const{id, value} = event.target;
+        const data = {
+            id : id,
+            value : value
+        }
+        setAnswer(data)
+    }
+
+    function submitAnswer(){
+        const data = {
+            id : match.params.id,
+            userAnswer : answer
+        }
+        console.log(data);
+    }
+
     if(!problem || !problem.contents){
         return null;
     }
@@ -28,9 +46,10 @@ import * as API from '../services/api'
             {problem.contents.map((content) =>(
                 <div key={content.id}>
                     <div>질문 : {content.question}</div>
-                    <label>답변 : </label> <input type="text" />
+                    <label>답변 : </label> <input id={content.id} type="text" name="userAnswer" value={content.useAnswer} onChange={onChange} />
                 </div>
             ))}
+            <button onClick={submitAnswer}>제출하기</button>
         </div>    
     )
 }
