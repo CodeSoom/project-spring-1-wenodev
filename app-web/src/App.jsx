362f0ed0c9896d0344941564a8e1022b8ea5ef91
weno <logicalstark1105@gmail.com>
2021-04-15 12:27:49 +0900

    fix: ProblemResult route 추가하라

--- app-web/src/App.jsx
+++ app-web/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ProblemList from './components/ProblemList';
 import ProblemDetail from './components/ProblemDetail';
 import ProblemForm from './components/ProblemForm';
+import ProblemResult from './components/ProblemResult';
 
 import { Route } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
       <Route exact path="/problems" component={ProblemList} />
       <Route exact path="/problems/:id" component={ProblemDetail} />
       <Route exact path="/form" component={ProblemForm} />
+      <Route exact path="/result" component={ProblemResult} />
     </div>
   );
 }
