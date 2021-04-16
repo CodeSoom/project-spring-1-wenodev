7f4d7ccdb6164bde2c38fea424132112065da422
weno <logicalstark1105@gmail.com>
2021-04-16 11:51:38 +0900

    feat: user 생성하라

---
+++ app-api/src/main/java/com/weno/user/User.java
@@ -0,0 +1,38 @@
+package com.weno.user;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@NoArgsConstructor
+@Entity
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String email;
+
+    @Column
+    private String name;
+
+    @Column
+    private String password;
+
+    @Builder
+    public User(Long id, String email, String name, String password) {
+        this.id = id;
+        this.email = email;
+        this.name = name;
+        this.password = password;
+    }
+}
