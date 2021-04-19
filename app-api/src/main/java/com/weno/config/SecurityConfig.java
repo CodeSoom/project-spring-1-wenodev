2d2be84cc074fec8178175b38c0f05e0bda1bb06
wenodev <logicalstark1105@gmail.com>
2021-04-20 08:02:54 +0900

    fix: SecurityConfig 설정 추가하라

--- app-api/src/main/java/com/weno/config/SecurityConfig.java
+++ app-api/src/main/java/com/weno/config/SecurityConfig.java
@@ -1,2 +1,32 @@
-package com.weno.config;public class SecurityConfig {
+package com.weno.config;
+
+import com.weno.auth.AuthService;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final AuthService authService;
+
+    public SecurityConfig(AuthService authService) {
+        this.authService = authService;
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers()
+                .frameOptions().disable()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .exceptionHandling()
+                .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED));
+    }
 }
