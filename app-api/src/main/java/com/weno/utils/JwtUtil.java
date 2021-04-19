0a949c168e1e85f1f4104c3af1ff75595e0ae566
weno <logicalstark1105@gmail.com>
2021-04-19 14:48:26 +0900

    feat: JwtUtil 생성하고 Key의 값을 확인하라

--- app-api/src/main/java/com/weno/utils/JwtUtil.java
+++ app-api/src/main/java/com/weno/utils/JwtUtil.java
@@ -1,2 +1,18 @@
-package com.weno.utils;public class JwtUtil {
+package com.weno.utils;
+
+import io.jsonwebtoken.security.Keys;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+
+@Component
+public class JwtUtil {
+
+    private final Key key;
+
+    public JwtUtil(@Value("${jwt.secret}") String secret) {
+        key = Keys.hmacShaKeyFor(secret.getBytes());
+    }
+
 }
--- app-api/src/test/java/com/weno/utils/JwtUtilTest.java
+++ app-api/src/test/java/com/weno/utils/JwtUtilTest.java
@@ -1,4 +1,17 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.weno.utils;
+
+import io.jsonwebtoken.security.Keys;
+import org.junit.jupiter.api.Test;
+
+import java.nio.charset.StandardCharsets;
+
 class JwtUtilTest {
-  
+
+    private final String SECRET = "12345678901234567890123456789010";
+
+    @Test
+    void testGetKey(){
+        System.out.println(Keys.hmacShaKeyFor(SECRET.getBytes(StandardCharsets.UTF_8)));
+    }
+
 }
