1b6cc8de9162207fe2ec68d73d3a2edf2f6ff8fd
weno <logicalstark1105@gmail.com>
2021-04-19 11:51:29 +0900

    feat: of 메소드 추가하라

--- app-api/src/main/java/com/weno/auth/dto/UserResultData.java
+++ app-api/src/main/java/com/weno/auth/dto/UserResultData.java
@@ -1,5 +1,6 @@
 package com.weno.auth.dto;
 
+import com.weno.user.User;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -8,16 +9,25 @@ import lombok.NoArgsConstructor;
 @Getter
 public class UserResultData {
     private Long id;
-    private String name;
     private String email;
+    private String name;
     private String password;
 
     @Builder
-    public UserResultData(Long id, String name, String email, String password) {
+    public UserResultData(Long id, String email, String name, String password) {
         this.id = id;
-        this.name = name;
         this.email = email;
+        this.name = name;
         this.password = password;
+    }
+
+    public static UserResultData of(User user){
+        return UserResultData.builder()
+                .id(user.getId())
+                .email(user.getEmail())
+                .name(user.getName())
+                .password(user.getPassword())
+                .build();
     }
 
 }
