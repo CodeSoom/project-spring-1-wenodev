0bae2facc6804bad67452d853b123f9b69c4ce0c
wenodev <logicalstark1105@gmail.com>
2021-04-20 07:59:50 +0900

    feat: JwtAuthenticationFilter 생성하라

---
+++ app-api/src/main/java/com/weno/filters/JwtAuthenticationFilter.java
@@ -0,0 +1,25 @@
+package com.weno.filters;
+
+import com.weno.auth.AuthService;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
+    private final AuthService authService;
+
+    public JwtAuthenticationFilter(AuthenticationManager authenticationManager, AuthService authService) {
+        super(authenticationManager);
+        this.authService = authService;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
+        String authorization = request.getHeader("Authorization");
+    }
+}
