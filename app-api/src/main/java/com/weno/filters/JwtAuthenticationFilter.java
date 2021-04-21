a2f79b3d8a88922eeccc65cdd673c46cbf16436e
weno <logicalstark1105@gmail.com>
2021-04-21 18:28:53 +0900

    fix: aceess토큰으로 부터 email을 얻고 유저를 인증하여 context에 등록하라

--- app-api/src/main/java/com/weno/filters/JwtAuthenticationFilter.java
+++ app-api/src/main/java/com/weno/filters/JwtAuthenticationFilter.java
@@ -1,7 +1,11 @@
 package com.weno.filters;
 
 import com.weno.auth.AuthService;
+import com.weno.role.Role;
+import com.weno.security.UserAuthentication;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -9,6 +13,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     private final AuthService authService;
@@ -21,5 +26,18 @@ public class JwtAuthenticationFilter extends BasicAuthenticationFilter {
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws IOException, ServletException {
         String authorization = request.getHeader("Authorization");
+
+        if (authorization != null){
+            String accessToken = authorization.substring("Bearer ".length());
+            String email = authService.parseToken(accessToken);
+            List<Role> roles = authService.roles(email);
+            UserAuthentication authentication = new UserAuthentication(roles,email);
+
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(authentication);
+        }
+
+        chain.doFilter(request, response);
+
     }
 }
