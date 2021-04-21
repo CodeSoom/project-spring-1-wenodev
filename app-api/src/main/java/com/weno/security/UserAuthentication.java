e8c79e4780702c7e74221d25344bba2b8ec44628
weno <logicalstark1105@gmail.com>
2021-04-21 18:16:30 +0900

    feat: UserAuthentication 생성하라

---
+++ app-api/src/main/java/com/weno/security/UserAuthentication.java
@@ -0,0 +1,40 @@
+package com.weno.security;
+
+import com.weno.role.Role;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class UserAuthentication extends AbstractAuthenticationToken {
+
+    private final String email;
+
+    public UserAuthentication(Collection<? extends GrantedAuthority> authorities, String email) {
+        super(authorities);
+        this.email = email;
+    }
+
+    @Override
+    public Object getCredentials() {
+        return null;
+    }
+
+    @Override
+    public Object getPrincipal() {
+        return null;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    private static List<GrantedAuthority> authorities(List<Role> roles){
+        return roles.stream()
+                .map(role -> new SimpleGrantedAuthority(role.getRoleName().name()))
+                .collect(Collectors.toList());
+    }
+}
