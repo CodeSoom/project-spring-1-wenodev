621e047737e0f4bbe4f218b9c8f929dccfa3142c
weno <logicalstark1105@gmail.com>
2021-04-22 18:32:59 +0900

    fix: EnableGlobalMethodSecurity 추가하라

--- app-api/src/main/java/com/weno/config/SecurityConfig.java
+++ app-api/src/main/java/com/weno/config/SecurityConfig.java
@@ -5,15 +5,18 @@ import com.weno.filters.AuthenticationErrorFilter;
 import com.weno.filters.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
+import javax.annotation.security.RolesAllowed;
 import javax.servlet.Filter;
 
 
 @Configuration
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
     private final AuthService authService;
 
