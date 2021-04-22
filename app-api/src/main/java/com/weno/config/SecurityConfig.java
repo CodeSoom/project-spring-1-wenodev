07cdf32d144f16c808a8efc9a991ccf2b3561b13
weno <logicalstark1105@gmail.com>
2021-04-22 11:09:00 +0900

    authenticationFilter 추가하라

--- app-api/src/main/java/com/weno/config/SecurityConfig.java
+++ app-api/src/main/java/com/weno/config/SecurityConfig.java
@@ -1,12 +1,16 @@
 package com.weno.config;
 
 import com.weno.auth.AuthService;
+import com.weno.filters.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+
+import javax.servlet.Filter;
+
 
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -18,11 +22,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+        Filter authenticationFilter =  new JwtAuthenticationFilter(authenticationManager(), authService);
+
         http
                 .csrf().disable()
                 .headers()
                 .frameOptions().disable()
                 .and()
+                .addFilter(authenticationFilter)
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
