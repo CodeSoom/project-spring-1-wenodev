57e82b60b6b28f996cf79e82f4194671e3fc9103
weno <logicalstark1105@gmail.com>
2021-04-22 16:43:49 +0900

    fix: 문제 출제는 로그인한 회원만 할 수 있도록 변경하라

--- app-api/src/main/java/com/weno/problem/ProblemController.java
+++ app-api/src/main/java/com/weno/problem/ProblemController.java
@@ -3,6 +3,7 @@ package com.weno.problem;
 import com.weno.problem.dto.ProblemRequestDto;
 import com.weno.problem.dto.ProblemResponseDto;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -35,6 +36,7 @@ public class ProblemController {
         return problemService.getProblem(id);
     }
 
+    @PreAuthorize("isAuthenticated() and hasAuthority('USER')")
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping
     public ProblemResponseDto saveProblem(@RequestBody ProblemRequestDto request){
