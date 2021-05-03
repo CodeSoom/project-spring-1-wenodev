94d5fb095544f2f06a9294caf1c5a0f1cfb905a2
weno <logicalstark1105@gmail.com>
2021-05-03 12:29:00 +0900

    feat: 로그인 요청하는 api 생성하라

--- app-web/src/components/LoginForm.jsx
+++ app-web/src/components/LoginForm.jsx
@@ -1,4 +1,5 @@
 import {useState} from 'react'
+import * as API from '../services/api';
 
 export default function LoginForm(){
     const [user, setUser] = useState({
@@ -14,6 +15,16 @@ export default function LoginForm(){
         })
     }
 
+    function submitUser(){
+        API.loginUser(user)
+        .then(response => {
+            console.log(response);
+        })
+        .catch(e => {
+            new Error(e);
+        })
+    }
+
     return(
     <div>
         <h2>로그인</h2>
@@ -21,5 +32,8 @@ export default function LoginForm(){
         <input type="text" value={user.email} name="email" placeholder="이메일을 입력하세요" onChange={onChange} />
         <label>비밀번호</label>
         <input type="password" value={user.password} name="password" placeholder="비밀번호를 입력하세요" onChange={onChange} />
+        <div>
+            <button type="submit" onClick={submitUser} >로그인</button>
+        </div>
     </div>)
 }
--- app-web/src/services/api.js
+++ app-web/src/services/api.js
@@ -50,3 +50,16 @@ export async function postUser(user){
   const data = await response.json();
   return data;
 }
+
+export async function loginUser(user){
+  const url = "http://localhost:8080/login";
+  const response = await fetch(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(user)
+  });
+  const data = await response.json();
+  return data;
+}
