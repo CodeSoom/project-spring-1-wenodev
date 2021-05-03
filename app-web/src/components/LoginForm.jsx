aa68636918207611f16bebc7c300f553faa0043f
weno <logicalstark1105@gmail.com>
2021-05-03 12:22:43 +0900

    feat: onChange 생성하라

--- app-web/src/components/LoginForm.jsx
+++ app-web/src/components/LoginForm.jsx
@@ -1,15 +1,25 @@
 import {useState} from 'react'
 
 export default function LoginForm(){
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    const [user, setUser] = useState({
+        email : "",
+        password : "",
+    });
+
+    function onChange(event){
+        const {name, value} = event.target;
+        setUser({
+            ...user,
+            [name]: value
+        })
+    }
 
     return(
     <div>
         <h2>로그인</h2>
         <label>이메일</label>
-        <input type="text" value={email} placeholder="이메일을 입력하세요" />
+        <input type="text" value={user.email} name="email" placeholder="이메일을 입력하세요" onChange={onChange} />
         <label>비밀번호</label>
-        <input type="password" value={password} placeholder="비밀번호를 입력하세요" />
+        <input type="password" value={user.password} name="password" placeholder="비밀번호를 입력하세요" onChange={onChange} />
     </div>)
 }
